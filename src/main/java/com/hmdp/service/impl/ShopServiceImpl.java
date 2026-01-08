d6d2f36cfba84ae0fa68b778c67e228f9ca53a07
Calista0429 <BaoxiHuang0429@gmail.com>
2026-01-08 16:25:29 +0900

    feat: Adding Redis caching for shop data

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,10 +9,14 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -41,7 +45,22 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.fail("商店不存在！");
         }
 
-        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        //更新数据库
+        updateById(shop);
+        Long shopId = shop.getId();
+        if (shopId == null) {
+            Result.fail("店铺id不能为空");
+        }
+        //清理缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shopId);
+        return Result.ok(shop);
+
     }
 }
